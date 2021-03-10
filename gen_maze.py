9ff76ad169e388a226d3ecd4b1b8b0af87cf42c8
Leah Moser <leahmoser@Leahs-MBP.home>
2021-03-09 19:02:48 -0800

    starting

--- gen_maze.py
+++ gen_maze.py
@@ -1,2 +1,15 @@
 #code for genetic maze solver
 import random 
+import math
+
+MAX_FITNESS = 100
+POSSIBLE_GENES = ["N", "E", "S", "W"]
+GENE_LENGTH = 9
+
+class Individual:
+
+    def __init__(self):
+        self.fitness = 0
+        self.gene = []
+        for x in range(Individual.GENE_LENGTH):
+            self.genes.append(random.choice(Individual.POSSIBLE_GENES)))
