97206fceff022cc380d8476c62125faf3d1fcec7
Sam Rind <orind@pdx.edu>
2021-03-09 19:25:16 -0800

    Fitness function added

--- gen_maze.py
+++ gen_maze.py
@@ -12,4 +12,16 @@ class Individual:
         self.fitness = 0
         self.gene = []
         for x in range(Individual.GENE_LENGTH):
-            self.genes.append(random.choice(Individual.POSSIBLE_GENES)))
+            self.genes.append(random.choice(Individual.POSSIBLE_GENES))
+
+    
+    def Fitness(self, curLoc, endLoc, maxMag):
+        x = curLoc[0] - endLoc[0]
+        y = curLoc[1] - endLoc[1]
+        x = x ** 2
+        y = y ** 2
+        mag = math.sqrt(x + y)
+        fitMod = 1.0 - (mag / maxMag) 
+        fitness = Individual.MAX_FITNESS * fitMod
+        fitness = int(round(fitness))
+        self.fitness = fitness
